782c8eb45a3924520c66799909188067396c03dc
Manorath Naphaphone <Naphaphonem@outlook.com>
2021-05-01 21:06:43 -0700

    routes

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
